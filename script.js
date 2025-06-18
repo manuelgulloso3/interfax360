09925b026ff8c3bf15cfdf93bc32189396717781
manuelgulloso3 <gullosomanuel3@gmail.com>
2025-06-18 16:29:47 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+// Puedes agregar aquí funciones adicionales si deseas interacción personalizada
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('Jagua Visión 360 - Página cargada correctamente');
+});
